92b3c768270d4f39b3ca17355204813f79592cb0
Bohdan99I <bohdan.voiyov@gmail.com>
2023-05-28 15:09:17 +0300

    Task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css'
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//Створюємо розмітку
+function createGalleryItem(galleryItems) {
+      return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"           
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+//Додаємо елементи img до HTML
+const items = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", items);
+
+//Працює бібліотека
+const lightboxGallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: 250,
+})
